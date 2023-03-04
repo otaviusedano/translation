5e0b06fa34d8fd64d33efe0077651d159d80a14b
otaviusedano <sedanootavio@gmail.com>
2023-03-04 13:06:29 -0300

    add: images

---
+++ src/components/Image/index.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react"
+import { IoIosCheckmark } from "react-icons/io"
+import Imgix from "react-imgix"
+
+import "./styles.scss"
+import "lazysizes"
+
+export function Image({ src, selecteds, isDowloaded }: any) {
+  const [isSelected, setIsSelectd] = useState(false)
+
+  function handleClick() {
+    setIsSelectd(!isSelected)
+
+    if (!selecteds.includes(src)) selecteds.push(src)
+    else {
+      const index = selecteds.indexOf(src)
+      if (index > -1) {
+        selecteds.splice(index, 1)
+      }
+    }
+
+    console.log(selecteds)
+    return
+  }
+
+  return (
+    <div className={`image ${isDowloaded ? "isDowloaded" : ""}`}>
+      <Imgix
+        imgixParams={{ fit: "crop", ar: "1:1" }}
+        sizes="(min-width: 960px) 33vw, (min-width: 340px) 50vw"
+        htmlAttributes={{
+          onClick: () => handleClick(),
+          src: "data-src",
+          srcSet: "data-srcset",
+          sizes: "data-sizes",
+        }}
+        className={`lazyload ${isSelected ? "selected" : ""}`}
+        src={src}
+      />
+      <IoIosCheckmark
+        onClick={() => handleClick()}
+        className={`icone ${isSelected ? "active" : ""}`}
+        fontSize={64}
+        color={"rgba(255, 255, 255, 0.87)"}
+      />
+    </div>
+  )
+}
